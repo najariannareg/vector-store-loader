8627e20b92db99c98731f0ec593b733cbd2db40f
najariannareg <43207695+najariannareg@users.noreply.github.com>
2025-06-22 14:18:08 +0400

    Initial commit. Chapter 4. Loading Documents.

---
+++ src/main/java/com/example/vectorstoreloader/VectorStoreLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.vectorstoreloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectorStoreLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VectorStoreLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vectorstoreloader/VectorStoreLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vectorstoreloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VectorStoreLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
